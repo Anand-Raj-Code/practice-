5d37cc094894e2c8fd3536079565aa269f8771d2
Anand-Raj <miryalkaranandkumar@gmail.com>
2026-07-02 22:48:33 +0530

    Added Sub file and added m2 method in Main class

--- BasicJava/src/main/java/com/anand/Main.java
+++ BasicJava/src/main/java/com/anand/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
            Sum s1=new Sum();
            System.out.println(s1.m1(11,12));
+           Sub s2=new Sub();
+           System.out.println(s2.m2(10,2));
     }
 }
---
+++ BasicJava/src/main/java/com/anand/Sub.java
@@ -0,0 +1,9 @@
+package com.anand;
+
+public class Sub {
+
+    public Integer m2(int a,int b){
+        int sub=a-b;
+        return  sub;
+    }
+}
